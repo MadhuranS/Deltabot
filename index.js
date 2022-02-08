86f100d0fd05087dd50bb36bde1fea512a4a9e3e
Madhu Sivapragasam <madhuran.siva@gmail.com>
2022-02-07 21:41:03 -0500

    add interaction handler

--- index.js
+++ index.js
@@ -138,3 +138,18 @@ client.on("messageCreate", (message) => {
 		return;
 	}
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+	const command = client.commands.get(interaction.commandName);
+	if (!command) return;
+	try {
+		await permissionHandler(interaction, command)
+	}
+	catch (error) {
+		error_logger(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.login(config.token);
